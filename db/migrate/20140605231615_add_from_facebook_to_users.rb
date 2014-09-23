3432a565555634d8aa9b8af4af53293c4440cb5c
fcheng <fcheng@adm-ml-c02mm24.ad.medctr.ucla.edu>
2014-09-23 15:09:34 -0700

    Fixing migration classname

--- db/migrate/20140605231615_add_from_facebook_to_users.rb
+++ db/migrate/20140605231615_add_from_facebook_to_users.rb
@@ -1,4 +1,4 @@
-class AddFromMobileToUsers < ActiveRecord::Migration
+class AddFromFacebookToUsers < ActiveRecord::Migration
   def change
   	add_column :users, :from_facebook, :boolean, :default => false
   end
