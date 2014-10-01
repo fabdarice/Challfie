994de73c65ab5190583aa38501c46e7562ebd4aa
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-10-01 05:01:04 -0700

    Add Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'challfie'
-set :repo_url, 'git@bitbucket.org:fabdarice/challfie_web.git'
+set :repo_url, 'git@challfie:fabdarice/challfie_web.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
