fe3e2c2438fa765507cac7fd4809a2f0fe5b98f8
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-10-01 15:50:16 -0700

    Try to fix Assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Challfie::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -43,7 +43,7 @@ Challfie::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
 
   config.action_mailer.default_url_options = { :host => 'challfie.herokuapp.com' }
 
