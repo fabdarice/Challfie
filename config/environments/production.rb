85b119ccfd27a84b0ad813a88104589b0c8f7e2d
fcheng <fcheng@adm-ml-c02mm24.ad.medctr.ucla.edu>
2014-10-08 11:51:48 -0700

    fix paperclip amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Challfie::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => ":s3_domain_url",
     :s3_credentials => {
       :bucket => ENV['CHALLFIE_AWS_BUCKET'],
       :access_key_id => ENV['CHALLFIE_AWS_ACCESS_KEY'],
