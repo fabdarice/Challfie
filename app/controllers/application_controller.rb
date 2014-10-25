9ed4a71c3ce5ef1d4b18b36aef1b5e22f032cb50
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-10-25 00:29:17 -0700

    DeSign Mobile Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def check_browser    
-    if !browser.mobile?
+    if browser.mobile?
       redirect_to mobile_path
     end    
   end
