b55373aa93b039b58496682f34c1827552b0ace5
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-10-30 01:37:28 -0700

    Try to force HTTPS for route first timer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Challfie::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'home#index'
+  root 'home#index', :constraints => { :protocol => "https" }
 
   get '/auto_refresh' => 'home#auto_refresh'
 
