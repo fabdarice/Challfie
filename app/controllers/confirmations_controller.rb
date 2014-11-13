687f6781ef616707e3c39f865b1a93a4af9aac79
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-11-13 02:16:57 -0800

    Remove Mobile page for password reset and confirmation email

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,5 @@
 class ConfirmationsController < Devise::ConfirmationsController
 	layout 'extra_pages'
-	skip_before_action :check_browser, only: [:create]
+	skip_before_action :check_browser, only: [:show]
 
 end
