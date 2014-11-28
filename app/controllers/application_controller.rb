319807017449825c99613f76da71fca983fb66e3
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-11-28 02:37:55 -0800

    Include ActiveController:Serializer in the app controller to use serialization_scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   include Rails.application.routes.url_helpers
+  include ActiveController::Serializer
+
   protect_from_forgery with: :null_session
   
   before_action :configure_permitted_parameters, if: :devise_controller?
