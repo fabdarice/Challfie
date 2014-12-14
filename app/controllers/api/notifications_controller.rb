f7191d603d4d9996dfd839d2a16215c1dabf3a44
fcheng <fcheng@ADM-ML-C02MM24.local>
2014-12-13 23:09:02 -0800

    fix Api/NotificationsController

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -6,5 +6,6 @@ module Api
     	def index
         @notifications = current_user.notifications.order('created_at DESC').paginate(:page => params[:page])
         render json: @notifications
+     	end
     end
 end
