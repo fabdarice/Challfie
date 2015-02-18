f0ab38dcf2502d2c70c84bb847d40d691c1cccdd
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-02-17 18:08:10 -0800

    Test redirect after Facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -36,9 +36,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], false)
 
       if @user              
-        #sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-        sign_in @user
-        redirect_to notifications_path
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        #sign_in @user
+        #redirect_to notifications_path
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?      
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
