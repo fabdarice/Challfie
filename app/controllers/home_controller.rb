797b37f25c62e5f14d426caac150f5f9b7ffca1b
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-04-25 16:45:25 -0700

    New HomePage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,11 +21,11 @@ class HomeController < ApplicationController
 		users_following = current_user.following(1)
  		list_following_ids = users_following.map{|u| u.id}
  		list_following_ids << current_user.id  
- 		@new_selfies = Selfie.where("user_id in (?) and created_at > ?", list_following_ids, Time.at(params[:after_selfie].to_i)).order("created_at DESC")
+ 		@new_selfies = Selfie.where("user_id in (?) and created_at > ?", list_following_ids, Time.at(params[:after_selfie].to_i)).order("created_at DESC").includes(:user, :challenge)
 
  		# REFRESH NOTIFICATIONS
  		time_reference = Time.at(Time.now.to_i - (params[:interval].to_i / 1000) - 2)
- 		@new_notifications = current_user.notifications.where("created_at > ?", time_reference).order('created_at DESC')
+ 		@new_notifications = current_user.notifications.where("created_at > ?", time_reference).order('created_at DESC').includes(:author, :selfie, :book)
 
  		# REFRESH FRIENDS REQUEST
  		user_followers = Follow.where('status = 0 and followable_id = ? and blocked = false and created_at > ?', current_user.id, time_reference)
