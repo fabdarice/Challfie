1fa23fdbabf782134796316c25d1909bee264da3
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-08-17 12:21:10 -0700

    oauth_expires_at not updating

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,15 +96,12 @@ class User < ActiveRecord::Base
       user.username_activated = false      
       user.skip_confirmation!  
     else  
-      logger.info "enter find_for_facebook_oauth"
-      logger.info "#{auth[:credentials][:expires_at]}"
-      logger.info "#{Time.at(auth[:credentials][:expires_at])}"
+      
       user.update_attributes(uid: auth[:uid],
                             provider: auth[:provider],
-                            oauth_token: Time.at(auth[:credentials][:token]),
+                            oauth_token: auth[:credentials][:token],
                             oauth_expires_at: auth[:credentials][:expires_at],
-                            facebook_picture: auth[:info][:image].gsub!("http", "https"))
-
+                            facebook_picture: auth[:info][:image].gsub!("http", "https")) 
     end
 
     if user.save                                   
