de87402795fcf4205b53b059d1645dec75925fee
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-08-19 01:03:12 -0700

    Add New PublishPermissions column to FacebookInfos

--- db/migrate/20150819065432_add_publish_permissions_to_facebook_infos.rb
+++ db/migrate/20150819065432_add_publish_permissions_to_facebook_infos.rb
@@ -1,5 +1,5 @@
 class AddPublishPermissionsToFacebookInfos < ActiveRecord::Migration
   def change
-  	 add_column :facebook_infos, :publish_permissions, :boolean, :default => true
+  	 add_column :facebook_infos, :publish_permissions, :boolean, :default => false
   end
 end
