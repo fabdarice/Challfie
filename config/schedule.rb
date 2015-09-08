dfd4af0524dc7970dde20b0dc7c5cbd20cf811d3
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-09-08 01:35:08 -0700

    Testing Daily Challenge

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,10 +21,10 @@
 set :output, "/home/deploy/challfie/shared/log/cron_log.log"
 
 
-every 1.day, :at => '8am' do
+every 1.day, :at => '1:40am' do
 	runner "DailyChallenge.new.set_daily_challenge_us"
 end
 
-every 1.day, :at => '11pm' do
+every 1.day, :at => '1:38am' do
 	runner "DailyChallenge.new.set_daily_challenge_fr"
 end
