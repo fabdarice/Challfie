bbb56c570e9e7e8cc8ba3b4ba35a8e15466ae4f6
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-09-08 01:47:34 -0700

    set Timer for Daily challenge

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,10 +21,10 @@
 set :output, "/home/deploy/challfie/shared/log/cron_log.log"
 
 
-every 1.day, :at => '4:46am' do
+every 1.day, :at => '8am' do
 	runner "DailyChallenge.new.set_daily_challenge_us"
 end
 
-every 1.day, :at => '4:44am' do
+every 1.day, :at => '11pm' do
 	runner "DailyChallenge.new.set_daily_challenge_fr"
 end
