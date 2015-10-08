c1dfb8f48c655655b5ab535e72c6a057ab2091ea
fcheng <fcheng@ADM-ML-C02MM24.local>
2015-10-07 22:22:52 -0700

    Fix Alert Index to display ordered by id DESC instead of created_at DESC

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -4,14 +4,14 @@ module Api
       respond_to :json
 
     	def index
-        @notifications = current_user.notifications.order('created_at DESC').paginate(:page => params[:page], :per_page => 20)
+        @notifications = current_user.notifications.order('id DESC').paginate(:page => params[:page], :per_page => 20)
         unread_notifications = current_user.notifications.where(read: 0)
         
         render json: @notifications, meta: {new_alert_nb: unread_notifications.count}
      	end
 
      	def refresh
-	      @notifications = current_user.notifications.where("id >= ? ", params[:last_alert_id]).order('created_at DESC')
+	      @notifications = current_user.notifications.where("id >= ? ", params[:last_alert_id]).order('id DESC')
 	      unread_notifications = current_user.notifications.where(read: 0)
 	      
 	      render json: @notifications, meta: {new_alert_nb: unread_notifications.count}
